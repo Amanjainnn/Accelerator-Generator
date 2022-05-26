f1752915b78415bdd280bb3cc91316bcb31e5f4d
amanjainnn <amanjain.jain621@gmail.com>
2022-05-26 21:39:17 +0530

    minor fixes

--- src/main/resources/angular/app.component.ts
+++ src/main/resources/angular/app.component.ts
@@ -1,1 +1,16 @@
-template
+import { Component } from '@angular/core';
+import { HelloWorldService } from './core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent   {
+  title = 'app2';
+
+  constructor(private helloWorldService: HelloWorldService){
+    helloWorldService.helloWorld().subscribe(message=>(alert(message)));
+
+  }
+}
--- src/main/resources/angular/app.module.ts
+++ src/main/resources/angular/app.module.ts
@@ -1,1 +1,20 @@
-template
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { ApiModule } from './core';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    ApiModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
